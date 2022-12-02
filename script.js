6cdd9a3a7e020d243a5ea6b58c35a932cbfba7a6
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-02 02:55:16 +0100

    added new file

---
+++ script.js
@@ -0,0 +1,19 @@
+var firstName = 'john';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job 
++ '. Is he married? ' + isMarried);
+age = 'three';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job 
++ '. Is he married? ' + isMarried);
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
