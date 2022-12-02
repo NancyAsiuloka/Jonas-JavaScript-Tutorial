0fb2fc5d85c40517301078b7755472bb3c2c9b23
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-02 03:14:59 +0100

    added javascript

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-var firstName = 'john';
-var age = 28;
+// var firstName = 'john';
+// var age = 28;
 
-console.log(firstName + ' ' + age);
+// console.log(firstName + ' ' + age);
 
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job 
-+ '. Is he married? ' + isMarried);
-age = 'three';
-job = 'driver';
+// console.log(firstName + ' is a ' + age + ' year old ' + job 
+// + '. Is he married? ' + isMarried);
+// age = 'three';
+// job = 'driver';
 
-alert(firstName + ' is a ' + age + ' year old ' + job 
-+ '. Is he married? ' + isMarried);
+// alert(firstName + ' is a ' + age + ' year old ' + job 
+// + '. Is he married? ' + isMarried);
 
-var lastName = prompt('what is his last name?');
-console.log(firstName + ' ' +  lastName);
+// var lastName = prompt('what is his last name?');
+// console.log(firstName + ' ' +  lastName);
+
+// Basic operators
+
+var now, yearJohn, yearMark;
+now = 2018
+yearJohn = now - 28;
+yearMark = now - 33;
+console.log(yearJohn);
+
+console.log(now + 2);
