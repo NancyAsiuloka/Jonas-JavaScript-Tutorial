fec98f0efc50f2c27d945e1585fbb957f068ba20
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-05 23:44:11 +0100

    added some statement

--- script.js
+++ script.js
@@ -173,6 +173,21 @@ switch (job){
             console.log(firstName + ' designs beautiful websites.');
 }
 
+age = 56;
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a boy.');
+}
+
 
 
 
