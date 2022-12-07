64e4fea5a43a858b604c5db95b411f4d9fa5c13b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-07 14:54:13 +0100

    added a function

--- script.js
+++ script.js
@@ -250,6 +250,14 @@ var ageMike = calculateAge(1940);
 var ageJane = calculateAge(1969);
 console.log(ageJohn, ageMike, ageJane);
 
+function yearsUntilRetirement(year, firstName){
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    console.log(firstName + ' retire in ' + retirement + ' years.');
+}
+
+yearsUntilRetirement(1990, 'John');
+
 
 
 
