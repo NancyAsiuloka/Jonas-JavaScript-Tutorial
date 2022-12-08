7e93b8c3ee7597405d62008edeb6eda6b4ef5c0c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-08 19:55:22 +0100

    added  object

--- script.js
+++ script.js
@@ -315,26 +315,112 @@
 
 // Coding challenge
 
-function tipCalculate(bill) {
-  var percentage;
+// function tipCalculate(bill) {
+//   var percentage;
 
-  if (bill < 50) {
-    percentage = .2;
-  } else if (bill >= 50 && bill < 200) {
-    percentage = .15;
-  } else  {
-    percentage = .1;
-  }l
-  return percentage * bill;
-}
+//   if (bill < 50) {
+//     percentage = .2;
+//   } else if (bill >= 50 && bill < 200) {
+//     percentage = .15;
+//   } else  {
+//     percentage = .1;
+//   }l
+//   return percentage * bill;
+// }
 
-var bills = [124, 48, 268];
-var tips = [tipCalculate(bills[0]),
-            tipCalculate(bills[1]),
-            tipCalculate(bills[2])];
+// var bills = [124, 48, 268];
+// var tips = [tipCalculate(bills[0]),
+//             tipCalculate(bills[1]),
+//             tipCalculate(bills[2])];
 
-var finalValues = [bills[0] + tips[0], 
-                    bills[1] + tips[1],
-                    bills[2] + tips[2]];
+// var finalValues = [bills[0] + tips[0], 
+//                     bills[1] + tips[1],
+//                     bills[2] + tips[2]];
 
-console.log(tips, finalValues);
+// console.log(tips, finalValues);
+
+
+// Object and properties
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1990,
+    family: ['jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false 
+};
+
+console.log(john);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
