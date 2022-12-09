4399c9150939327f930ecb4ea6a3af7bb035b411
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-09 09:49:58 +0100

    added fuction expression

--- script.js
+++ script.js
@@ -241,24 +241,42 @@
 
 // Functions
 
-function calculateAge(birthYear){
-    return 2018 - birthYear;
+// function calculateAge(birthYear){
+//     return 2018 - birthYear;
+// }
+
+// var ageJohn = calculateAge(1990);
+// var ageMike = calculateAge(1940);
+// var ageJane = calculateAge(1969);
+// console.log(ageJohn, ageMike, ageJane);
+
+// function yearsUntilRetirement(year, firstName){
+//     var age = calculateAge(year);
+//     var retirement = 65 - age;
+
+//     if (retirement > 0) {
+//         console.log(firstName + ' retires in ' + 
+//         retirement + ' years.');
+//     } else {
+//         console.log(firstName + ' is already retired.');
+//     }
+   
+// }
+
+// yearsUntilRetirement(1990, 'John');
+// yearsUntilRetirement(1948, 'Mike');
+// yearsUntilRetirement(1969, 'Jane');
+
+// function statements and expressions
+
+// function declaration
+function whatDoYouDo(job, firstName){}
+
+
+// function expressions
+var whatDoYouDo = function(job, firstName){
+
 }
-
-var ageJohn = calculateAge(1990);
-var ageMike = calculateAge(1940);
-var ageJane = calculateAge(1969);
-console.log(ageJohn, ageMike, ageJane);
-
-function yearsUntilRetirement(year, firstName){
-    var age = calculateAge(year);
-    var retirement = 65 - age;
-    console.log(firstName + ' retire in ' + retirement + ' years.');
-}
-
-yearsUntilRetirement(1990, 'John');
-yearsUntilRetirement(1948, 'Mike');
-yearsUntilRetirement(1969, 'Jane');
 
 
 
