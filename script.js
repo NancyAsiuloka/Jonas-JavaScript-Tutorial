ecc27500b0d1ca40becb1f111c8801aa4119ca1e
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-09 16:10:02 +0100

    commented some code

--- script.js
+++ script.js
@@ -295,18 +295,39 @@
 // ARRAYS
 
 // Initialize new array
-var names = ['John', 'Mark', 'Jane'];
-var years = new Array(1990, 1969, 1948); 
+// var names = ['John', 'Mark', 'Jane'];
+// var years = new Array(1990, 1969, 1948); 
 
-console.log(names[0]);
-console.log(names.length);
+// console.log(names[0]);
+// console.log(names.length);
 
-// Mutate array data
-names[1] = 'Ben';
-names[names.length] = 'Mary';
+// // Mutate array data
+// names[1] = 'Ben';
+// names[names.length] = 'Mary';
 
-console.log(names);
+// console.log(names);
 
+// // Different data types
+
+// var john = ['John', 'Smith', 1990, 'designer', false];
+
+// john.push('blue');
+// john.unshift('Mr.')
+// console.log(john);
+
+// john.pop();
+// john.pop();
+// john.shift();
+// console.log(john);
+
+// console.log(john.indexOf(1990));
+
+// var isDesigner = john.indexOf('designer') === -1 ?  'John is NOT a designer': 
+// 'John IS a designer';
+
+// console.log(isDesigner);
+
+// Coding challenge
 
 
 
