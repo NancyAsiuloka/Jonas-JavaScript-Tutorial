5477d5b3681f17829892dff6f49862b3adfba165
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-17 08:26:49 +0100

    added a comment

--- script.js
+++ script.js
@@ -25,32 +25,32 @@
 
 // // Basic operators
 
-var year, yearJohn, yearMark;
-now = 2018;
+// var year, yearJohn, yearMark;
+// now = 2018;
 
 
-ageJohn = 28;
-ageMark = 33;
+// ageJohn = 28;
+// ageMark = 33;
 
-// math operators
-yearJohn = now - ageJohn;
-yearMark = now - ageMark;
-console.log(yearJohn);
+// // math operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+// console.log(yearJohn);
 
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 2);
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 2);
 
 // // Logical operators
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
+// var johnOlder = ageJohn < ageMark;
+// console.log(johnOlder);
 
 // // typeof operator
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof 'Mark is older than john');
-var x;
-console.log(typeof x);
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than john');
+// var x;
+// console.log(typeof x);
 
 // // operator precedence
 // var noe = 2018;
