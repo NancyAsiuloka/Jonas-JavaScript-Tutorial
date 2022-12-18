26de2558e7a8cc9fe4d04fb86ab5eb2ae20f1b31
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-18 08:31:05 +0100

    commented a code

--- script.js
+++ script.js
@@ -53,13 +53,13 @@
 // console.log(typeof x);
 
 // // operator precedence
-var now = 2018;
-var yearJohn = 1989;
-var fullAge = 18;
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
 
-// multiple operator
-var isFullAge = now - yearJohn >= fullAge;
-console.log(isFullAge);
+// // multiple operator
+// var isFullAge = now - yearJohn >= fullAge;
+// console.log(isFullAge);
 
 // // grouping
 // var ageJohn = now - yearJohn;
