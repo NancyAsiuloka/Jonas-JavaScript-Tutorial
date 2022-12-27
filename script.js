b85e3f7a079b3ed6a90829919401c2120aa3930d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-27 20:56:46 +0100

    added function expression

--- script.js
+++ script.js
@@ -270,22 +270,22 @@ var age = 14;
 // function whatDoYouDo(job, firstName){}
 
 // function expressions
-// var whatDoYouDo = function(job, firstName){
-//     switch(job) {
-//         case 'teacher':
-//             return firstName + ' teaches kid how to code';
-//         case 'driver':
-//             return firstName + ' drives a car in Lisbon.';
-//         case 'designer':
-//             return firstName + ' designs beautiful websites';
-//         default:
-//             return firstName + ' does something else';
-//     }
-// }
+var whatDoYouDo = function(job, firstName){
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kid how to code';
+        case 'driver':
+            return firstName + ' drives a car in Lisbon.';
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
 
-// console.log(whatDoYouDo('teacher', 'John'));
-// console.log(whatDoYouDo('designer', 'Jane'));
-// console.log(whatDoYouDo('retired', 'Mike'));
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mike'));
 
 // ARRAYS
 
