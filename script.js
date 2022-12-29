7397937f44837d9ad6130ea488a2906a2475c069
Nancy Asiuloka <nancyasiuloka@gmail.com>
2022-12-29 17:31:10 +0100

    added some code

--- script.js
+++ script.js
@@ -551,6 +551,107 @@
 //     console.log(mark.lastName + '\'s family pays higher tips, with an average of $' + mark.average);
 // }
 
+var john = {
+    fullName: 'John Smith',
+    birthYear: 1990,
+    bills: [124, 48, 268, 180, 42],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for(var i = 0; i < this.bills.length; i++){
+            var percentage;
+            var bill = this.bills[i];
+
+            if(bill < 50){
+                percentage = .2;
+            } else if (bill >= 50 && bill < 200){
+                percentage = .15;
+            } else {
+                percentage = .1;
+            }
+
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+};
+
+john.calcTips();
+
+
+
+var mark = {
+    fullName: 'Mark Miler',
+    birthYear: 1990,
+    bills: [77, 375, 110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+
+        for(var i = 0; i < this.bills.length; i++){
+            var percentage;
+            var bill = this.bills[i];
+
+            if(bill < 100){
+                percentage = .2;
+            } else if (bill >= 100 && bill < 300){
+                percentage = .1;
+            } else {
+                percentage = .25;
+            }
+
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+};
+
+// john.calcTips();
+// mark.calcTips();
+// console.log(john, mark);
+
+
+
+function calcAverage(tips){
+    var sum = 0;
+    for(var i = 0; i < tips.length; i++){
+        sum = sum + tips[i];
+    } 
+    return sum / tips.length;
+}
+john.calcTips();
+mark.calcTips();
+
+john.average = calcAverage(john.tips);
+mark.average = calcAverage(mark.tips);
+console.log(john, mark);
+
+
+if(john.average > mark.average){
+        console.log(john.fullName + '\'s family pays higher tips, with an average of $' + john.average);
+    } else {
+        console.log(mark.fullName + '\'s family pays higher tips, with an average of $' + mark.average);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
