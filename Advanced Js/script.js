41b117a5679894dbabc606b1565a00877cf70d88
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-01-06 08:15:25 +0100

    added a file

--- Advanced Js/script.js
+++ Advanced Js/script.js
@@ -12,6 +12,23 @@ var Person = function(name, yearOfBirth, job){
     this.job = job;
 }
 
+Person.prototype.calculateAge = function(){
+    console.log(2016 - this.yearOfBirth);
+}
+
+
+
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+
+
+
+
 
 
